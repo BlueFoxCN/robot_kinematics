b997a11efc45fb3bd62fb3cf618075ee8fef1c68
JesseYang <jesse.yang@1985@gmail.com>
2018-11-15 12:34:16 +0800

    more solutions

--- robot_kinematics.py
+++ robot_kinematics.py
@@ -7,6 +7,9 @@ def vecs_angle(vec1, vec2):
 def angle2rad(angle):
     return angle * np.pi / 180
 
+def rad2angle(rad):
+    return rad * 180 / np.pi
+
 def forward_kinematics(phi_angle_ary):
     theta_bias_ary = np.array([0, angle2rad(90), angle2rad(-90), 0, 0, 0])
     phi_ary = np.array([angle2rad(e) for e in phi_angle_ary])
@@ -50,9 +53,7 @@ def forward_kinematics(phi_angle_ary):
     for T in T_list[5:]:
         pt1 = np.matmul(T, pt1)
 
-    # print("forward A:")
-    # print(pt5)
-
+    '''
     print("forward points:")
     print(pt1)
     print(pt2)
@@ -60,9 +61,7 @@ def forward_kinematics(phi_angle_ary):
     print(pt4)
     print(pt5)
     print(pt6)
-
-    import pdb
-    pdb.set_trace()
+    '''
 
     dir_vec = pt6 - pt5
     dir_vec = dir_vec / np.linalg.norm(dir_vec)
@@ -73,7 +72,6 @@ def inverse_kinematics(coord):
     d0 = 320
     d1 = 140
     d2 = 265
-    # for debug, d3 is set to 0, should be 20
     d3 = 20
     d4 = 343
     d5 = 156
@@ -101,28 +99,52 @@ def inverse_kinematics(coord):
 
     phi_1_minus_2 = np.mod(Bphi_1 - Bphi_2, 2*np.pi)
     if phi_1_minus_2 < np.pi:
-        B = np.array([Bx_1, By_1, Bz])
+        B_list = [np.array([Bx_1, By_1, Bz]),
+                  np.array([Bx_1, By_1, Bz]),
+                  np.array([Bx_2, By_2, Bz]),
+                  np.array([Bx_2, By_2, Bz])]
     else:
-        B = np.array([Bx_2, By_2, Bz])
+        B_list = [np.array([Bx_2, By_2, Bz]),
+                  np.array([Bx_2, By_2, Bz]),
+                  np.array([Bx_1, By_1, Bz]),
+                  np.array([Bx_1, By_1, Bz])]
 
-    print("inverse A: ")
-    print(A)
-    print("inverse B: ")
-    print(B)
+    def norm_neg_pi_2_pos_pi(rad):
+        return (rad + np.pi) % (2 * np.pi) - np.pi
 
-    phi_1_range = [-170 * np.pi / 180, 170 * np.pi / 180]
-    phi_1 = np.arctan2(B[1], B[0])
-    phi_1 = (phi_1 + np.pi) % (2 * np.pi) - np.pi
-
-    if not (phi_1_range[0] <= phi_1 <= phi_1_range[1]):
-        raise ValueError()
+    phi_1_list = []
+    for idx in range(len(B_list)):
+        if idx in [0, 1]:
+            phi_1_list.append(norm_neg_pi_2_pos_pi(np.arctan2(B_list[idx][1], B_list[idx][0])))
+        else:
+            phi_1_list.append(norm_neg_pi_2_pos_pi(np.arctan2(B_list[idx][1], B_list[idx][0]) + np.pi))
 
     # calculate phi_2 and phi_3, should have two pair of solutions
-    Dx = np.sqrt(B[0]**2 + B[1]**2) - d1
-    Dy = A[2] - d0
+    D_list = []
+    for idx in range(len(B_list)):
+        if idx in [0, 1]:
+            Dx = np.sqrt(B_list[idx][0]**2 + B_list[idx][1]**2) - d1
+        else:
+            Dx = np.sqrt(B_list[idx][0]**2 + B_list[idx][1]**2) + d1
+        Dy = A[2] - d0
+        D_list.append(np.array([Dx, Dy]))
 
-    cos_phi_3 = (Dx**2 + Dy**2 - d2**2 - d4**2) / (2 * d2 * d4)
-    phi_3 = np.arccos(cos_phi_3)
+    if np.sqrt(Dx**2 + Dy**2) > d2 + d4:
+        D_list = D_list[:2]
+        phi_1_list = phi_1_list[:2]
+
+    def calculate_phi_3(D):
+        cos_phi_3 = (D[0]**2 + D[1]**2 - d2**2 - d4**2) / (2 * d2 * d4)
+        phi_3 = np.arccos(cos_phi_3)
+        return [phi_3, -phi_3]
+
+    phi_3_list = []
+    for idx in range(len(D_list)):
+        if idx % 2 == 0:
+            cur_phi_3_list = calculate_phi_3(D_list[idx])
+            # if idx >= 2:
+            #     cur_phi_3_list = [-e for e in cur_phi_3_list]
+            phi_3_list += cur_phi_3_list
 
     def sin_phi_2(phi_3):
         return -((d4 * np.cos(phi_3) + d2) * Dx + d4 * np.sin(phi_3) * Dy) / \
@@ -131,28 +153,35 @@ def inverse_kinematics(coord):
     def cos_phi_2(phi_3, s_phi_2):
         return (Dy + d4 * np.sin(phi_3) * s_phi_2) / (d4 * np.cos(phi_3) + d2)
 
-    s_phi_2 = sin_phi_2(phi_3)
-    c_phi_2 = cos_phi_2(phi_3, s_phi_2)
-    phi_3_1 = phi_3
-    phi_2_1 = np.arctan2(s_phi_2, c_phi_2)
+    def calculate_phi_2(phi_3):
+        s_phi_2 = sin_phi_2(phi_3)
+        c_phi_2 = cos_phi_2(phi_3, s_phi_2)
+        phi_2 = np.arctan2(s_phi_2, c_phi_2)
+        return phi_2
 
-    s_phi_2 = sin_phi_2(-phi_3)
-    c_phi_2 = cos_phi_2(-phi_3, s_phi_2)
-    phi_3_2 = -phi_3
-    phi_2_2 = np.arctan2(s_phi_2, c_phi_2)
+    phi_2_list = []
+    for idx in range(len(phi_3_list)):
+        phi_2_list.append(calculate_phi_2(phi_3_list[idx]))
 
+    for idx in range(len(phi_2_list)):
+        if idx >= 2:
+            # when the direction of target and the first joint is opposite
+            phi_2_list[idx] = -phi_2_list[idx]
+            phi_3_list[idx] = -phi_3_list[idx]
 
     # calculate point E's coordinate
-    E1_x = B[0] * (np.sqrt(B[0]**2 + B[1]**2) + d4 * np.sin(phi_2_1 + phi_3_1)) / np.sqrt(B[0]**2 + B[1]**2)
-    E1_y = B[1] * (np.sqrt(B[0]**2 + B[1]**2) + d4 * np.sin(phi_2_1 + phi_3_1)) / np.sqrt(B[0]**2 + B[1]**2)
-    E2_x = B[0] * (np.sqrt(B[0]**2 + B[1]**2) + d4 * np.sin(phi_2_2 + phi_3_2)) / np.sqrt(B[0]**2 + B[1]**2)
-    E2_y = B[1] * (np.sqrt(B[0]**2 + B[1]**2) + d4 * np.sin(phi_2_2 + phi_3_2)) / np.sqrt(B[0]**2 + B[1]**2)
-    E1_z = A[2] - d4 * np.cos(phi_2_1 + phi_3_1)
-    E2_z = A[2] - d4 * np.cos(phi_2_2 + phi_3_2)
+    def calculate_E(A, B, phi_2, phi_3):
+        Ex = B[0] * (np.sqrt(B[0]**2 + B[1]**2) + d4 * np.sin(phi_2 + phi_3)) / np.sqrt(B[0]**2 + B[1]**2)
+        Ey = B[1] * (np.sqrt(B[0]**2 + B[1]**2) + d4 * np.sin(phi_2 + phi_3)) / np.sqrt(B[0]**2 + B[1]**2)
+        Ez = A[2] - d4 * np.cos(phi_2 + phi_3)
+        return np.array([Ex, Ey, Ez])
 
-    E1 = np.array([E1_x, E1_y, E1_z])
-    E2 = np.array([E2_x, E2_y, E2_z])
-
+    E_list = []
+    for idx in range(len(phi_2_list)):
+        if idx < 2:
+            E_list.append(calculate_E(A, B_list[idx], phi_2_list[idx], phi_3_list[idx]))
+        else:
+            E_list.append(calculate_E(A, B_list[idx], -phi_2_list[idx], -phi_3_list[idx]))
 
     # calculate phi_5
     def calculate_phi_5(E, B, A, T):
@@ -172,8 +201,9 @@ def inverse_kinematics(coord):
 
         return phi_5
 
-    phi_5_1 = calculate_phi_5(np.array([E1_x, E1_y, E1_z]), B, A, tcp)
-    phi_5_2 = calculate_phi_5(np.array([E2_x, E2_y, E2_z]), B, A, tcp)
+    phi_5_list = []
+    for idx in range(len(E_list)):
+        phi_5_list.append(calculate_phi_5(E_list[idx], B_list[idx], A, tcp))
 
     # calculate point F's coordinate
     def calculate_F(A, B, E, phi_5):
@@ -182,12 +212,14 @@ def inverse_kinematics(coord):
         Fz = A[2] + np.cos(phi_5) * d5 * (B[2] - E[2]) / d4
         return np.array([Fx, Fy, Fz])
 
-    F1 = calculate_F(A, B, E1, phi_5_1)
-    F2 = calculate_F(A, B, E2, phi_5_2)
+    F_list = []
+    for idx in range(len(E_list)):
+        F_list.append(calculate_F(A, B_list[idx], E_list[idx], phi_5_list[idx]))
 
     # calculate beta
-    beta_1 = np.arcsin((B[2] - E1[2]) / d4)
-    beta_2 = np.arcsin((B[2] - E2[2]) / d4)
+    beta_list = []
+    for idx in range(len(E_list)):
+        beta_list.append(np.arcsin((B_list[idx][2] - E_list[idx][2]) / d4))
 
     # calculate point T_prime's coordinate
     def calculate_T_prime(A, F, phi_5, beta):
@@ -196,8 +228,9 @@ def inverse_kinematics(coord):
         T_prime_z = A[2] + d5 * np.sin(phi_5 + beta)
         return np.array([T_prime_x, T_prime_y, T_prime_z])
 
-    T_prime_1 = calculate_T_prime(A, F1, phi_5_1, beta_1)
-    T_prime_2 = calculate_T_prime(A, F2, phi_5_2, beta_2)
+    T_prime_list = []
+    for idx in range(len(F_list)):
+        T_prime_list.append(calculate_T_prime(A, F_list[idx], phi_5_list[idx], beta_list[idx]))
 
     # calculate phi_4
     def calculate_phi_4(A, F, T, T_prime):
@@ -217,23 +250,36 @@ def inverse_kinematics(coord):
 
         return phi_4
 
-    phi_4_1 = calculate_phi_4(A, F1, tcp, T_prime_1)
-    phi_4_2 = calculate_phi_4(A, F2, tcp, T_prime_2)
+    phi_4_list = []
+    for idx in range(len(F_list)):
+        phi_4 = calculate_phi_4(A, F_list[idx], tcp, T_prime_list[idx])
+        if idx >= 2:
+            phi_4 = norm_neg_pi_2_pos_pi(phi_4 + np.pi)
+        phi_4_list.append(phi_4)
 
-    phi_1_angle = phi_1 * 180 / np.pi
-    phi_2_1_angle = phi_2_1 * 180 / np.pi
-    phi_2_2_angle = phi_2_2 * 180 / np.pi
-    phi_3_1_angle = phi_3_1 * 180 / np.pi
-    phi_3_2_angle = phi_3_2 * 180 / np.pi
-    phi_4_1_angle = phi_4_1 * 180 / np.pi
-    phi_4_2_angle = phi_4_2 * 180 / np.pi
-    phi_5_1_angle = phi_5_1 * 180 / np.pi
-    phi_5_2_angle = phi_5_2 * 180 / np.pi
+    def list_rad2angle(rad_list):
+        angle_list = [rad2angle(e) for e in rad_list]
+        return angle_list
 
-    sol_1 = np.array([phi_1_angle, phi_2_1_angle, phi_3_1_angle, phi_4_1_angle, phi_5_1_angle])
-    sol_2 = np.array([phi_1_angle, phi_2_2_angle, phi_3_2_angle, phi_4_2_angle, phi_5_2_angle])
+    phi_1_angle_list = list_rad2angle(phi_1_list)
+    phi_2_angle_list = list_rad2angle(phi_2_list)
+    phi_3_angle_list = list_rad2angle(phi_3_list)
+    phi_4_angle_list = list_rad2angle(phi_4_list)
+    phi_5_angle_list = list_rad2angle(phi_5_list)
 
-    return [sol_1, sol_2]
+
+    sol_list = []
+    for idx in range(len(phi_1_angle_list)):
+        sol_list.append(np.array([phi_1_angle_list[idx],
+                                  phi_2_angle_list[idx],
+                                  phi_3_angle_list[idx],
+                                  phi_4_angle_list[idx],
+                                  phi_5_angle_list[idx]]))
+
+    print("solutions:")
+    for sol in sol_list:
+        print(np.around(sol, 3))
+    return sol_list
 
 if __name__ == "__main__":
     for idx in range(1):
@@ -247,8 +293,6 @@ if __name__ == "__main__":
         phi_angle_ary = [94.354, 42.293, 27.137, 22.269, -45.978, 0]
         print(np.around(phi_angle_ary[:5], 3))
         ret = forward_kinematics(phi_angle_ary)
-        print("tcp: ")
-        print(ret[:3])
+        # print("tcp and direction:")
+        # print(np.around(ret, 3))
         sol_list = inverse_kinematics(np.array(ret))
-        for sol in sol_list:
-            print(np.around(sol, 3))
